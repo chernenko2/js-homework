37f7b5e8d1f5990694b5ed9607b9fb30dc716865
chernenko2 <oleh.chernenko97@gmail.com>
2022-10-09 12:30:01 +0300

    third homework done

---
+++ sandbox.js
@@ -0,0 +1,34 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+// function areYouPlayingBanjo(name) {
+//   if (name[0].toLowerCase() == "r") {
+//     name += " plays banjo";
+//   } else {
+//     name += " does not play banjo";
+//   }
+//   return name;
+// }
+
+// areYouPlayingBanjo("Red");
+
+let arr = [1, 2, 3, 4, 5];
+// let a = arr.splice(2);
+// console.log(a);
+// console.log(arr);
+// let b = arr.splice(2, 2);
+// console.log(b);
+
+// перша цифра то з якого елементу, друга цифра то скільки видалити, третя цифра і інші то які числа додати в масив
+// arr.splice(3, 1, 25, "35", true, undefined, null, [1, 2], {});
+// arr.push(10, 15);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
